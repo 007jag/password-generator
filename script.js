ac3d13f3a69b06a1b76f74633b63878ec9ccf7b4
007jag <114781974+007jag@users.noreply.github.com>
2022-11-10 14:49:50 -0500

    added randomized functions

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max){
+  if (!max){
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomItem (list) {
+  return list [randomInt(list.length)]
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
-  passwordText.value = password;
+  if (password){
+      passwordText.value = password;
+  }
+  
 
 }
 
